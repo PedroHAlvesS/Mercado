ddc17bd276ada7e6d4db00461369654be1bc4009
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-07-26 15:31:42 -0300

    patch itens do pedido

--- src/main/java/br/com/compass/Sprint05/dto/pedido/PedidoItensDto.java
+++ src/main/java/br/com/compass/Sprint05/dto/pedido/PedidoItensDto.java
@@ -2,7 +2,10 @@ package br.com.compass.Sprint05.dto.pedido;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class PedidoItensDto {
+    @NotNull
     private Long itemId;
 }
