9a8c8ca613d470e60aa95387f22403136911ff29
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-08-09 15:25:37 -0300

    Validando Cliente

--- API Site/src/main/java/br/com/compass/site/dto/cliente/request/RequestClienteDto.java
+++ API Site/src/main/java/br/com/compass/site/dto/cliente/request/RequestClienteDto.java
@@ -5,6 +5,7 @@ import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Data
 public class RequestClienteDto {
@@ -12,6 +13,7 @@ public class RequestClienteDto {
     @NotBlank
     private String cpf;
     @NotBlank
-    @Length(min = 3)
+    @Length(min = 3, max = 150)
+    @Pattern(regexp="^[A-Za-z]*$",message = "Nome invalido")
     private String nome;
 }
