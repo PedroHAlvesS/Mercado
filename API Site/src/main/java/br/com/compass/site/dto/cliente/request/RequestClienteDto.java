df0e731fd9feae6fa819b8634b612a719d240c8e
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-08-09 11:16:36 -0300

    Remove imports nao usados

--- API Site/src/main/java/br/com/compass/site/dto/cliente/request/RequestClienteDto.java
+++ API Site/src/main/java/br/com/compass/site/dto/cliente/request/RequestClienteDto.java
@@ -4,7 +4,6 @@ import lombok.Data;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 @Data
 public class RequestClienteDto {
