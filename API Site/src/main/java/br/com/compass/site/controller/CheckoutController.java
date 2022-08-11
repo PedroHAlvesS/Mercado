69358a3cce73d6ab9a81d8e631d2f7887f67cd6a
PedroAS <85406215+PedroHAlvesS@users.noreply.github.com>
2022-08-11 12:12:49 -0300

    fix path: checkout

--- API Site/src/main/java/br/com/compass/site/controller/CheckoutController.java
+++ API Site/src/main/java/br/com/compass/site/controller/CheckoutController.java
@@ -9,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
@@ -21,9 +20,9 @@ public class CheckoutController {
     private final CheckoutService checkoutService;
 
     @PostMapping
-    public ResponseEntity<ResponseCheckoutDto> criaCheckoutDosItens(@Valid @RequestBody RequestCheckoutDto requestDto, UriComponentsBuilder uriBuilder) {
+    public ResponseEntity<ResponseCheckoutDto> criaCheckoutDosItens(@Valid @RequestBody RequestCheckoutDto requestDto) {
         ResponseCheckoutDto responseDto = checkoutService.enviaPedido(requestDto);
-        URI uri = uriBuilder.path("http://localhost:8080/api/pedido/{id}").buildAndExpand(responseDto.getNumeroDoPedido()).toUri();
+        URI uri = URI.create("http://localhost:8080/api/pedido/" + responseDto.getNumeroDoPedido());
         return ResponseEntity.created(uri).body(responseDto);
     }
 }
